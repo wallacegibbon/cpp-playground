37e8b69eb98e87204d4dc638d68343e286b24310
wallace <wallacegibbon@outlook.com>
2022-05-11 18:23:18 +0800

    examples for raw string literal

---
+++ miscellaneous/raw_string_literal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, const char **argv) {
+    // R"(....)"
+    std::cout << R"(hello, this is a "raw string literal".)" << std::endl;
+
+    // "\" will not escape special characters inside raw string literal
+    std::cout << R"(hello\t\t\nworld".)" << std::endl;
+
+    // R"d-char-sequences(raw-char-sequences)d-char-sequences" is another useful syntax
+    std::cout << R"blah(hello, this is another "raw string literal".)blah" << std::endl;
+
+    // you can cantain )" in this syntax
+    std::cout << R"blah(there can be )" now )blah" << std::endl;
+
+    return 0;
+}
