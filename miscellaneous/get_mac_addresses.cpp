d2ce349857a93daeae19cd3537b81cc6d9ca3ddf
wallace <wallacegibbon@outlook.com>
2022-05-17 16:32:41 +0800

    example for std::filesystem operation.

---
+++ miscellaneous/get_mac_addresses.cpp
@@ -0,0 +1,30 @@
+// the std::filesystem need c++17 support, to compile this file:
+//  g++ --std=c++17 miscellaneous/get_mac_addresses.cpp
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <set>
+
+std::string get_address_from_device(std::filesystem::path device_path) {
+    std::ifstream input(device_path / "address");
+    std::string result;
+    input >> result;
+    return result + "@" + device_path.filename().string();
+}
+
+std::set<std::string> GetMacAddresses() {
+    std::set<std::string> result;
+    for (const auto &f : std::filesystem::directory_iterator("/sys/class/net")) {
+        result.insert(get_address_from_device(f.path()));
+    }
+    return result;
+}
+
+int main(int argc, const char **argv) {
+    std::set<std::string> mac_addresses = GetMacAddresses();
+    for (auto &addr : mac_addresses) {
+        std::cout << addr << std::endl;
+    }
+    return 0;
+}
